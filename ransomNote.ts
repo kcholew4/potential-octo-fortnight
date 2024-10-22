04966c8869ba046a3f76c93d40f0fb51eab33bc4
Kamil Cholewa <40357022+kcholew4@users.noreply.github.com>
2024-10-22 14:40:57 +0200

    chore: add comments

--- ransomNote.ts
+++ ransomNote.ts
@@ -4,13 +4,16 @@ export const checkRansomNote = (message: string, magazine: string) => {
   const messageLetterOccurrences = getLetterOccurrences(message);
   const magazineLetterOccurrences = getLetterOccurrences(magazine);
 
+  // Iterate over every character in the message and the amount of times it has occured
   for (const [letter, count] of messageLetterOccurrences.entries()) {
     const magazineLetterCount = magazineLetterOccurrences.get(letter);
 
+    // A character doesn't exist in the magazine
     if (!magazineLetterCount) {
       return false;
     }
 
+    // A character occurs more times than in the magazine.
     if (count > magazineLetterCount) {
       return false;
     }
